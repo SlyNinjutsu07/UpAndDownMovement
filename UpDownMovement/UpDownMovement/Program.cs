dc603d2a35aab9dd209daa35587f9519f49cd645
Sly <85970858+SlyNinjutsu07@users.noreply.github.com>
2024-04-02 09:14:54 -0400

    Update Program.cs
    Got rid of former ReadInput method and game functioning

--- UpDownMovement/UpDownMovement/Program.cs
+++ UpDownMovement/UpDownMovement/Program.cs
@@ -15,43 +15,6 @@ namespace UpDownMovement
 
             Game.Play(g);
 
-#if false
-            string? input = null;
-
-            while(input != "ESC")
-            {
-                input = ReadLine();
-                ReadInput(input, l);
-            }
-            
-            WriteLine("Thanks for playing!");
-#endif
-
-
-        }
-
-        public static void ReadInput(string? input, Level level)
-        {
-            switch (input?.ToLower())
-            {
-                //  0,0   0,1   0,2   0,3   0,4
-                //  1,0   1,1   1,2   1,3   1,4
-                //  2,0   2,1   2,2   2,3   2,4
-                //  3,0   3,1   3,2   3,3   3,4
-                //  4,0   4,1   4,2   4,3   4,4
-                case "w":
-                    level.Move(-1, 0);
-                    break;
-                case "a":
-                    level.Move(0, -1);
-                    break;
-                case "s":
-                    level.Move(1, 0);
-                    break;
-                case "d":
-                    level.Move(0, 1);
-                    break;
-            }
         }
     }
 }
